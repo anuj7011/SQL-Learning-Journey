a0c8f2406f3a848e9c92b83caecf7d4201b609eb
Anuj <195161484+anuj7011@users.noreply.github.com>
2026-07-07 15:12:50 +0530

    Add SQL select queries for employees table

---
+++ Day-01-SELECT/select_queries.sql
@@ -0,0 +1,14 @@
+-- Select all columns
+
+SELECT *
+FROM employees;
+
+-- Select specific columns
+
+SELECT name, salary
+FROM employees;
+
+-- Select employee names only
+
+SELECT name
+FROM employees;
