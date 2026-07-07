dae0bb49d8f635dcd009311b343bd7d89ef82849
Anuj <195161484+anuj7011@users.noreply.github.com>
2026-07-07 15:46:15 +0530

    Create _ORDER_BY.sql

---
+++ ORDERBY/_ORDER_BY.sql
@@ -0,0 +1,19 @@
+-- ==========================================
+-- Topic : ORDER BY
+-- Purpose : Sort data in ascending or descending order.
+-- ==========================================
+
+-- Ascending Order
+SELECT *
+FROM employees
+ORDER BY salary ASC;
+
+-- Descending Order
+SELECT *
+FROM employees
+ORDER BY salary DESC;
+
+-- Sort by Name
+SELECT *
+FROM employees
+ORDER BY name ASC;
