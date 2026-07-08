91e76471f4a9df1752e19d0dee35232a712bc65b
Anuj <195161484+anuj7011@users.noreply.github.com>
2026-07-08 20:40:41 +0530

    Update ALTER_TABLE.sql

--- 12-ALTER-TABLE/ALTER_TABLE.sql
+++ 12-ALTER-TABLE/ALTER_TABLE.sql
@@ -1,1 +1,18 @@
+-- Add new column
+ALTER TABLE employees
+ADD email VARCHAR(100);
 
+
+-- Modify column datatype
+ALTER TABLE employees
+ALTER COLUMN salary DECIMAL(10,2);
+
+
+-- Rename column
+ALTER TABLE employees
+RENAME COLUMN name TO employee_name;
+
+
+-- Drop column
+ALTER TABLE employees
+DROP COLUMN email;
